2e1b8325607f86493ae60e19f7b09cc0c1bd4afd
giuseppesiciliano <giuseppesiciliano90@gmail.com>
2022-01-17 11:41:24 +0100

    scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        ciao
+    </header>
+</template>
+
+
+<script>
+export default {
+  name: "Header",
+  
+}
+</script>
+
+
+<style lang="scss" scoped>
+header {
+    height: 80px;
+    background-color: pink;
+}
+</style>
